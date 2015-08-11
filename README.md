3c1ee7bd1010b6724b07fed7bdf340bbb64f75da
Sofia4041 <353124127@qq.com>
2015-08-11 23:04:45 +0800

    V1.0
    circleImageview,RecImageView,SquareImageview

---
+++ README.md
@@ -0,0 +1,7 @@
+
+job expereience
+
+simpleimageview contains circleImageview,RecImageView,SquareImageview,all of this just set attr.
+
+![](https://github.com/wanliLiu/SimpleImageView/raw/master/images/dem0.png)
+
---
+++ library/src/main/java/com/soli/simpleimageview/library/SimpleImageView.java
@@ -0,0 +1,121 @@
+package com.soli.simpleimageview.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.widget.ImageView;
+
+/**
+ * Created by soli on 15/8/11.
+ */
+public class SimpleImageView extends ImageView {
+
+    private final RectF roundRect = new RectF();
+    private final float defaultRadius = 6;
+    private float radius = 6;
+    private final Paint maskPaint = new Paint();
+    private final Paint zonePaint = new Paint();
+
+    private boolean isCycle = false;
+    private boolean isSquare = false;
+    private float heightPercent = 0.0f;
+
+    public SimpleImageView(Context context) {
+        this(context,null);
+    }
+
+    public SimpleImageView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public SimpleImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+
+        TypedArray a = getContext().obtainStyledAttributes(attrs,R.styleable.SimpleImageView);
+        if (a != null)
+        {
+            radius = a.getDimension(R.styleable.SimpleImageView_radius, radius);
+            heightPercent = a.getFloat(R.styleable.SimpleImageView_heightPercent, 0.0f);
+            isCycle = a.getBoolean(R.styleable.SimpleImageView_isCycle,false);
+            isSquare = a.getBoolean(R.styleable.SimpleImageView_isSquare,false);
+            if (isCycle || isSquare)
+            {
+                heightPercent = 1.0f;
+            }
+
+        }
+        a.recycle();
+    }
+
+     /**
+     *
+     */
+    private void init() {
+        maskPaint.setAntiAlias(true);
+        maskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        //
+        zonePaint.setAntiAlias(true);
+        zonePaint.setColor(Color.WHITE);
+        //
+        radius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, defaultRadius, getResources().getDisplayMetrics());
+    }
+
+    /**
+     *
+     * @param adius
+     */
+    public void setRectAdius(float adius) {
+        radius = adius;
+        invalidate();
+    }
+
+    public void setHeightPercent(float heightPercent) {
+        this.heightPercent = heightPercent;
+        invalidate();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right,
+                            int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        roundRect.set(0, 0, getWidth(), getHeight());
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.saveLayer(roundRect, zonePaint, Canvas.ALL_SAVE_FLAG);
+        if (isCycle)
+        {
+            canvas.drawOval(roundRect,zonePaint);
+        }else
+        {
+            canvas.drawRoundRect(roundRect, radius, radius, zonePaint);
+        }
+
+        canvas.saveLayer(roundRect, maskPaint, Canvas.ALL_SAVE_FLAG);
+        super.draw(canvas);
+        canvas.restore();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        if (heightPercent > 0.0f)
+        {
+            int width = MeasureSpec.getSize(widthMeasureSpec);
+            int height = (int)(width * heightPercent);
+            setMeasuredDimension(width,height);
+        }else
+        {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+}
